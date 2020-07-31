9615ef3102a7ad312cf94a1f01c0fee6ff6c533e
tracer223 <joshuahenderson031@gmail.com>
2020-07-30 19:31:53 -0700

    Updates

---
+++ precache-manifest.6354a794465bd2bed18bd24e1f93bdb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "07bf5edc21c132602c17",
+    "url": "/drum-machine/static/css/main.3371539f.chunk.css"
+  },
+  {
+    "revision": "07bf5edc21c132602c17",
+    "url": "/drum-machine/static/js/main.07bf5edc.chunk.js"
+  },
+  {
+    "revision": "baef5ad6cf75b14673f5",
+    "url": "/drum-machine/static/js/1.baef5ad6.chunk.js"
+  },
+  {
+    "revision": "3399999d14c61ad309c6",
+    "url": "/drum-machine/static/js/runtime~main.3399999d.js"
+  },
+  {
+    "revision": "ba44ded0b2d05a63b7c5da5b21dd295b",
+    "url": "/drum-machine/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1eb724f576da7061399b39c330618d17.js"
+  "/drum-machine/precache-manifest.6354a794465bd2bed18bd24e1f93bdb1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/drum-machine/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
